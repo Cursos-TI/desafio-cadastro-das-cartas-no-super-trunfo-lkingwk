1bc840463f8f5b0a4d96db61c836fbea5ed1c1e4
lkingwk <williamjeffersonwj15@gmail.com>
2025-02-21 13:49:06 +0000

    agr vai

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-//teste02 b
+//teste02 b2
 //inicio do desafio
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
+//teste will
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
